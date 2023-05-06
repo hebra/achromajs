85d819e166a5e6f606cf9e5d271b4a0483fdcb44
Hendrik Brandt <heb@20khz.de>
2023-05-06 17:58:03 +1000

    Update dependencies

--- release.config.js
+++ release.config.js
@@ -1,23 +1,23 @@
 module.exports = {
     branches: [
-        'master'
+        "master"
     ],
     plugins: [
-        '@semantic-release/commit-analyzer',
-        '@semantic-release/release-notes-generator',
+        "@semantic-release/commit-analyzer",
+        "@semantic-release/release-notes-generator",
         [
-            '@semantic-release/changelog',
+            "@semantic-release/changelog",
             {
-                changelogFile: 'CHANGELOG.md'
+                changelogFile: "CHANGELOG.md"
             }
         ],
-        '@semantic-release/npm',
-        '@semantic-release/github',
+        "@semantic-release/npm",
+        "@semantic-release/github",
         [
-            '@semantic-release/git',
+            "@semantic-release/git",
             {
-                assets: ['CHANGELOG.md', 'dist/**'],
-                message: 'chore(release): set `package.json` to ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}'
+                assets: ["CHANGELOG.md", "dist/**"],
+                message: "chore(release): set `package.json` to ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}"
             }
         ]
     ]
